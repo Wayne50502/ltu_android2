d7649ba8f76e5840a1688cb565177c4d525315ab
TankIsDog <wayne50504@gmail.com>
2017-11-22 14:34:27 +0800

    v1

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -6,13 +6,16 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fmgr;
     private ViewPager viewPager;
     private Page1 page1;
     private Page2 page2;
     private Page3 page3;
     private Page4 page4;
+    private Fragment[] pages;
 
 
     @Override
@@ -20,11 +23,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fmgr = getSupportFragmentManager();
         page1 = new Page1(); page2 = new Page2(); page3 = new Page3(); page4 = new Page4();
+        pages = new Fragment[]{page1, page2, page3, page4};
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
-        //viewPager.setAdapter(new MyPagerAdapter());
+        viewPager.setAdapter(new MyPagerAdapter(fmgr));
     }
 
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
@@ -35,12 +40,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return null;
+            return pages[position];
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return pages.length;
         }
+    }
+    public void gotoPage1(View view){
+
+    }
+    public void gotoPage2(View view){
+
+    }
+    public void gotoPage3(View view){
+
+    }
+    public void gotoPage4(View view){
+
     }
 }
